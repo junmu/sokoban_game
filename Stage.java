1300a6d88abf3ef4b5718189e054fdaa5cc6c25f
jinan159 <wlsdhks0423@naver.com>
2021-12-06 20:39:05 +0900

    [1단계] Stage, StageUtils 클래스 추가
    Stage 클래스는 스테이지의 모든 정보를 담고 있는 클래스
    StageUtils 클래스는 Stage 정보 변환 등 유틸 기능을 담고 있는 클래스

---
+++ Stage.java
@@ -0,0 +1,86 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Stage {
+    private char[][] chrMap;
+    private int[][] intMap;
+    private int stageIndex;
+
+    private Point mapSize;
+    private Point playerLocation;
+    private int hallCount;
+    private int ballCount;
+
+    public Stage(List<String> lines, int stageIndex) {
+        if (lines.size() < 3) {
+            throw new IllegalArgumentException("올바르지 않은 맵 정보입니다.");
+        }
+
+        // 문자열을 숫자 값으로 변환하여 맵 생성
+        this.chrMap = StageUtils.convertToChrMap(lines);
+        this.intMap = StageUtils.convertToIntMap(lines);
+        this.stageIndex = stageIndex;
+
+        setGameMapData(intMap);
+    }
+
+    private void setGameMapData(int[][] argMap) {
+        int height = argMap.length;
+        int width = argMap[0].length;
+
+        for (int y=0; y<argMap.length; y++) {
+            if (width < argMap[y].length) width = argMap[y].length;
+            for (int x=0; x<argMap[0].length; x++) {
+                int value = argMap[y][x];
+
+                if (!Sign.isValidValue(value)) throw new IllegalStateException("맵 정보에 올바르지 않은 문자가 포함되었습니다. [" + value + "]");
+
+                if (Sign.HALL.getValue() == value) hallCount++;
+                if (Sign.BALL.getValue() == value) ballCount++;
+                if (Sign.PLAYER.getValue() == value) playerLocation = new Point(x, y);
+            }
+        }
+
+        mapSize = new Point(width, height);
+    }
+
+    public char[][] getCloneChrMap() {
+        char[][] cloneMap = new char[mapSize.getY()][mapSize.getX()];
+
+        for (int y=0; y<cloneMap.length; y++) {
+            cloneMap[y] = Arrays.copyOf(chrMap[y], chrMap[y].length);
+        }
+
+        return cloneMap;
+    }
+
+    public int[][] getCloneIntMap() {
+        int[][] cloneMap = new int[mapSize.getY()][mapSize.getX()];
+
+        for (int y=0; y<cloneMap.length; y++) {
+            cloneMap[y] = Arrays.copyOf(intMap[y], intMap[y].length);
+        }
+
+        return cloneMap;
+    }
+
+    public int getStageIndex() {
+        return stageIndex;
+    }
+
+    public Point getMapSize() {
+        return mapSize;
+    }
+
+    public Point getPlayerLocation() {
+        return playerLocation;
+    }
+
+    public int getHallCount() {
+        return hallCount;
+    }
+
+    public int getBallCount() {
+        return ballCount;
+    }
+}
---
+++ StageUtils.java
@@ -0,0 +1,34 @@
+import java.util.List;
+
+public class StageUtils {
+    public static char[][] convertToChrMap(List<String> lines) {
+        char[][] map = new char[lines.size()][];
+
+        for (int i=0; i<map.length; i++) {
+            map[i] = lines.get(i).toCharArray();
+        }
+
+        return map;
+    }
+
+    public static int[][] convertToIntMap(List<String> lines) {
+        int[][] map = new int[lines.size()][];
+
+        for (int i=0; i<map.length; i++) {
+            map[i] = convertToIntRow(lines.get(i));
+        }
+
+        return map;
+    }
+
+    public static int[] convertToIntRow(String row) {
+        char[] chars = row.toCharArray();
+        int[] intRow = new int[chars.length];
+
+        for (int i=0; i<chars.length; i++) {
+            intRow[i] = Sign.convertMeanToValue(chars[i]);
+        }
+
+        return intRow;
+    }
+}
