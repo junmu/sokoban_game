34e8fab9fb46d955ee9741cb88a9238dbe7e50fd
jinan159 <wlsdhks0423@naver.com>
2021-12-06 23:46:16 +0900

    [2단계] UserCommand에 direction 추가
    사용자 입력에 따라 이동해야 하는 방향 좌표를 미리 설정함

--- UserCommand.java
+++ UserCommand.java
@@ -1,14 +1,24 @@
 import java.util.Arrays;
 
 public enum UserCommand {
-    W, // 위
-    A, // 왼쪽
-    S, // 아래
-    D, // 오른쪽
-    Q; // 프로그램 종료
+    W(new Point(0, -1)), // 위
+    A(new Point(-1, 0)), // 왼쪽
+    S(new Point(0, 1)),  // 아래
+    D(new Point(1, 0)),  // 오른쪽
+    Q(new Point(0, 0));  // 프로그램 종료
+
+    private Point direction;
+
+    UserCommand(Point point) {
+        this.direction = point;
+    }
 
     public static boolean isValidCommand(String inputCommand) {
         return Arrays.stream(UserCommand.values())
                 .anyMatch(userCommand -> userCommand.name().equals(inputCommand.toUpperCase()));
+    }
+
+    public Point getDirection() {
+        return direction;
     }
 }
