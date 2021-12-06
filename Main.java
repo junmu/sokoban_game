bb14830b17cffe994eee9af74b4dc7f523902a88
jinan159 <wlsdhks0423@naver.com>
2021-12-06 22:51:56 +0900

    [1단계] Main 리펙토링
    printAll -> printError로 명칭 정정
    gameMap -> stage로 명칭 정정
    printAllData 메소드에서 반복문까지 수행하도록 개선

--- Main.java
+++ Main.java
@@ -2,34 +2,30 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        boolean printAll = false;
-        if (args.length > 0 && args[0].equals("PRINT_ERROR")) printAll = true;
+        boolean printError = (args.length > 0 && args[0].equals("PRINT_ERROR"));
 
         try {
             StageReader reader = new CmdStageReader();
-            List<Stage> gameMapList = reader.readAllGameMaps();
-            StringBuilder sb = new StringBuilder();
-
-            for (Stage map : gameMapList) {
-                printAllData(map);
-            }
-
+            List<Stage> stageList = reader.readAllGameMaps();
+            printAllData(stageList);
         } catch (Exception e) {
-            if (printAll) e.printStackTrace();
+            if (printError) e.printStackTrace();
             System.out.println(e.getMessage());
             System.out.println("예기치 않게 종료되었습니다.");
         }
     }
 
-    private static void printAllData(Stage stage) {
-        System.out.printf("Stage %d\n\n", stage.getStageIndex());
-        System.out.print(stage.chrMapToString());
-        System.out.printf("가로 크기 : %d\n", stage.getMapSize().getX());
-        System.out.printf("세로 크기 : %d\n", stage.getMapSize().getY());
-        System.out.printf("구멍의 수 : %d\n", stage.getHallCount());
-        System.out.printf("공의 수 : %d\n", stage.getBallCount());
-        int y = stage.getPlayerLocation().getY() + 1;
-        int x = stage.getPlayerLocation().getX() + 1;
-        System.out.printf("플레이어 위치 (%d, %d)\n\n", y, x);
+    private static void printAllData(List<Stage> stageList) {
+        for (Stage stage : stageList) {
+            System.out.printf("Stage %d\n\n", stage.getStageIndex());
+            System.out.print(stage.chrMapToString());
+            System.out.printf("가로 크기 : %d\n", stage.getMapSize().getX());
+            System.out.printf("세로 크기 : %d\n", stage.getMapSize().getY());
+            System.out.printf("구멍의 수 : %d\n", stage.getHallCount());
+            System.out.printf("공의 수 : %d\n", stage.getBallCount());
+            int y = stage.getPlayerLocation().getY() + 1;
+            int x = stage.getPlayerLocation().getX() + 1;
+            System.out.printf("플레이어 위치 (%d, %d)\n\n", y, x);
+        }
     }
 }
