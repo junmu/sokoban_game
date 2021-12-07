3efa67f5570b49b3bbf8ff69c1f92f042b6889a2
jinan159 <wlsdhks0423@naver.com>
2021-12-07 15:31:05 +0900

    [2단계] UserCommand에 출력 메세지 추가
    Enum에 명령어에 따른 출력 메세지 추가 및 사용자 입력에 맞는 enum 찾는 메소드 추가

--- UserCommand.java
+++ UserCommand.java
@@ -1,24 +1,37 @@
 import java.util.Arrays;
+import java.util.Optional;
 
 public enum UserCommand {
-    W(new Point(0, -1)), // 위
-    A(new Point(-1, 0)), // 왼쪽
-    S(new Point(0, 1)),  // 아래
-    D(new Point(1, 0)),  // 오른쪽
-    Q(new Point(0, 0));  // 프로그램 종료
+    W(new Point(0, -1), "위로 이동합니다."), // 위
+    A(new Point(-1, 0), "왼쪽으로 이동합니다."), // 왼쪽
+    S(new Point(0, 1), "아래로 이동합니다."),  // 아래
+    D(new Point(1, 0), "오른쪽으로 이동합니다."),  // 오른쪽
+    Q(new Point(0, 0), "프로그램을 종료합니다.");  // 프로그램 종료
 
     private Point direction;
+    private String message;
 
-    UserCommand(Point point) {
+    UserCommand(Point point, String message) {
         this.direction = point;
+        this.message = message;
     }
 
     public static boolean isValidCommand(String inputCommand) {
         return Arrays.stream(UserCommand.values())
-                .anyMatch(userCommand -> userCommand.name().equals(inputCommand.toUpperCase()));
+                .anyMatch(userCommand -> userCommand.name().equalsIgnoreCase(inputCommand));
+    }
+
+    public static Optional<UserCommand> findUserCommand(char inputCommand) {
+        return Arrays.stream(UserCommand.values())
+                .filter(userCommand -> userCommand.name().equalsIgnoreCase(String.valueOf(inputCommand)))
+                .findAny();
     }
 
     public Point getDirection() {
         return direction;
+    }
+
+    public String getMessage() {
+        return message;
     }
 }
