893d4faad48998ae7a4eb46c58a3c9017f07589f
jinan159 <wlsdhks0423@naver.com>
2021-12-07 22:37:02 +0900

    [3단계] R 명령어(스테이지 초기화) 추가

--- UserCommand.java
+++ UserCommand.java
@@ -2,11 +2,12 @@ import java.util.Arrays;
 import java.util.Optional;
 
 public enum UserCommand {
-    W(new Point(0, -1), "위로 이동합니다."), // 위
-    A(new Point(-1, 0), "왼쪽으로 이동합니다."), // 왼쪽
-    S(new Point(0, 1), "아래로 이동합니다."),  // 아래
-    D(new Point(1, 0), "오른쪽으로 이동합니다."),  // 오른쪽
-    Q(new Point(0, 0), "프로그램을 종료합니다.");  // 프로그램 종료
+    W(new Point(0, -1), "위로 이동합니다."),        // 위
+    A(new Point(-1, 0), "왼쪽으로 이동합니다."),     // 왼쪽
+    S(new Point(0, 1), "아래로 이동합니다."),       // 아래
+    D(new Point(1, 0), "오른쪽으로 이동합니다."),    // 오른쪽
+    Q(new Point(0, 0), "프로그램을 종료합니다."),    // 프로그램 종료
+    R(new Point(0, 0), "스테이지를 초기화 합니다.");  // 스테이지 초기화
 
     private Point direction;
     private String message;
