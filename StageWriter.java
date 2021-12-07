d0af7252218d5c101a1a3212ef47dad935e9c6f9
jinan159 <wlsdhks0423@naver.com>
2021-12-07 13:12:30 +0900

    [2단계] StageWriter 메소드 명칭 정정

--- StageWriter.java
+++ StageWriter.java
@@ -1,9 +1,9 @@
 import java.util.List;
 
 public interface StageWriter {
-    void writeGameMap(char[][] chrMap) throws Exception;
-    void writeGameMap(Stage stage) throws Exception;
-    void writeAllGameMaps(List<Stage> stageList) throws Exception;
+    void writeStage(char[][] chrMap) throws Exception;
+    void writeStage(Stage stage) throws Exception;
+    void writeAllStages(List<Stage> stageList) throws Exception;
     void close() throws Exception;
     boolean isClosed();
 }
