2dd12dfd3e7e8cfb5bb647230522f9801847b7b8
jinan159 <wlsdhks0423@naver.com>
2021-12-07 21:53:59 +0900

    [3단계] Sign에 BALL_IN_HALL 추가
    BALL이 HALL안에 들어가있는 상태를 추가함

--- Sign.java
+++ Sign.java
@@ -6,7 +6,8 @@ public enum Sign {
     HALL('O', 1),
     BALL('o', 2),
     PLAYER('P', 3),
-    STAGE_DIV('=', 4);
+    STAGE_DIV('=', 4),
+    BALL_IN_HALL('0', 5);
 
     private char mean;
     private int value;
