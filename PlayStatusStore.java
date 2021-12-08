e9147c590df827e3f7f0987071ba56dbf93115aa
jinan159 <wlsdhks0423@naver.com>
2021-12-08 16:17:59 +0900

    [4단계] PlayStatusStore 리팩토링
    - 저장소를 배열로 변경
    - 저장시 PlayStatus의 복제본을 저장하도록 개선

--- PlayStatusStore.java
+++ PlayStatusStore.java
@@ -1,30 +1,33 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 public class PlayStatusStore {
     public static final int MIN_SLOT = 1;
     public static final int MAX_SLOT = 5;
 
-    private final List<PlayStatus> playStatusStore;
+    private final PlayStatus[] playStatusStore;
 
     public PlayStatusStore() {
         int size = MAX_SLOT - MIN_SLOT + 1;
-        playStatusStore = new ArrayList<>(size);
+        playStatusStore = new PlayStatus[size];
     }
 
     public boolean saveStatus(PlayStatus status, int index) {
         if (index >= MIN_SLOT && index <= MAX_SLOT) {
-            playStatusStore.add(index, status);
+            playStatusStore[index-1] = new PlayStatus.PlayStatusBuilder()
+                    .setStage(status.getStage())
+                    .setPlayer(new Position(status.getPlayer()))
+                    .setPlayingMap(status.getClonePlayingMap())
+                    .setPlayerMoveCount(status.getPlayerMoveCount())
+                    .setSuccess(status.isSuccess())
+                    .setQuit(status.isQuit())
+                    .build();
             return true;
         }
         return false;
     }
 
     public Optional<PlayStatus> loadStatus(int index) {
-        return playStatusStore.stream()
-                .filter(status -> playStatusStore.indexOf(status) == index)
-                .findAny();
+        return Optional.of(playStatusStore[index-1]);
     }
 
     public boolean isAvailableSlot(int index) {
