192a4d7a48bffde34368f55efc1d6888e2e38e14
jinan159 <wlsdhks0423@naver.com>
2021-12-08 19:14:21 +0900

    [4단계] PlayerCommand 역방향 추가
    Undo기능 구현에 필요한 입력의 역방향을 추가하였음

--- PlayerCommand.java
+++ PlayerCommand.java
@@ -2,16 +2,18 @@ import java.util.Arrays;
 import java.util.Optional;
 
 public enum PlayerCommand {
-    w(new Point(0, -1), "위로 이동합니다."),        // 위
-    a(new Point(-1, 0), "왼쪽으로 이동합니다."),     // 왼쪽
-    s(new Point(0, 1), "아래로 이동합니다."),       // 아래
-    d(new Point(1, 0), "오른쪽으로 이동합니다.");    // 오른쪽
+    w(new Point(0, -1), new Point(0, 1), "위로 이동합니다."),        // 위
+    a(new Point(-1, 0), new Point(1, 0), "왼쪽으로 이동합니다."),     // 왼쪽
+    s(new Point(0, 1), new Point(0, -1), "아래로 이동합니다."),       // 아래
+    d(new Point(1, 0), new Point(-1, 0), "오른쪽으로 이동합니다.");    // 오른쪽
 
     private Point direction;
+    private Point reverse;
     private String message;
 
-    PlayerCommand(Point point, String message) {
-        this.direction = point;
+    PlayerCommand(Point direction, Point reverse, String message) {
+        this.direction = direction;
+        this.reverse = reverse;
         this.message = message;
     }
 
@@ -28,6 +30,10 @@ public enum PlayerCommand {
 
     public Point getDirection() {
         return direction;
+    }
+
+    public Point getReverse() {
+        return reverse;
     }
 
     public String getMessage() {
