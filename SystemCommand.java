f6abc60efe0dbdbf9ae4eeb594da8741ac2f32c7
jinan159 <wlsdhks0423@naver.com>
2021-12-08 13:46:18 +0900

    [4단계] SystemCommand에 진행상황 저장, 불러오기 명령어 추가

--- SystemCommand.java
+++ SystemCommand.java
@@ -2,8 +2,10 @@ import java.util.Arrays;
 import java.util.Optional;
 
 public enum SystemCommand {
-    Q("프로그램을 종료합니다."),   // 프로그램 종료
-    R("스테이지를 초기화 합니다."); // 스테이지 초기화
+    Q("프로그램을 종료합니다."),             // 프로그램 종료
+    R("스테이지를 초기화 합니다."),          // 스테이지 초기화
+    S("세이브 슬롯에 진행상황을 저장합니다."),  // 스테이지 저장
+    L("세이브 슬롯에서 진행상황을 불러옵니다."); // 스테이지 로드
 
     private String message;
 
