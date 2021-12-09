cb854516321fb05a44b19337b24fd4aa07f3dbc4
jinan159 <wlsdhks0423@naver.com>
2021-12-09 16:38:08 +0900

    [4단계] 암호화된 map파일로 출력되도록 개선
    암호화된 파일로 Stage를 읽어서 게임을 실행함
    만약, 암호화된 파일이 없으면 map.txt를 읽어서 저장한 뒤 게임 실행함

--- Main.java
+++ Main.java
@@ -1,20 +1,58 @@
+import java.io.File;
 import java.util.List;
 
 public class Main {
+    private static final String MAP_FILE = "map.txt";
+    private static final String ENCRYPTED_MAP_FILE = "map_enc.txt";
+
     public static void main(String[] args) {
         boolean printError = (args.length > 0 && args[0].equals("PRINT_ERROR"));
 
         try {
-            String workingDir = System.getProperty("user.dir");
-            StageReader reader = new FileStageReader(workingDir + "/map.txt");
-            List<Stage> stageList = reader.readAllStages();
-            SokobanGame game = new SokobanGame(stageList);
-            game.startFirstStage();
+            gameStart();
         } catch (Exception e) {
             if (printError) e.printStackTrace();
             System.out.println(e.getMessage());
             System.out.println("예기치 않게 종료되었습니다.");
         }
+    }
+
+    private static void gameStart() throws Exception {
+        String encryptedMap = getCurrentDirectory() + ENCRYPTED_MAP_FILE;
+
+        File file = new File(encryptedMap);
+        if (!file.exists()) readAndWriteEncryptedMap(file);
+
+        List<Stage> stageList = readEncryptedStage(file.getAbsolutePath());
+        SokobanGame game = new SokobanGame(stageList);
+        game.startFirstStage();
+    }
+
+    private static void readAndWriteEncryptedMap(File file) throws Exception {
+        String map = getCurrentDirectory() + MAP_FILE;
+
+        List<Stage> stageList = readStage(map);
+        writeEncryptedStage(file.getAbsolutePath(), stageList);
+    }
+
+    private static List<Stage> readStage(String fullPath) throws Exception {
+        StageReader reader = new FileStageReader(fullPath);
+        return reader.readAllStages();
+    }
+
+    private static List<Stage> readEncryptedStage(String fullPath) throws Exception {
+        StageReader reader = new AES256FileStageReader(fullPath);
+        return reader.readAllStages();
+    }
+
+    private static void writeEncryptedStage(String fullPath, List<Stage> stageList) throws Exception {
+        StageWriter writer = new AES256FileStageWriter(fullPath);
+        writer.writeAllStages(stageList);
+        writer.close();
+    }
+
+    private static String getCurrentDirectory() {
+        return System.getProperty("user.dir") + "/";
     }
 
     private static void printAllData(List<Stage> stageList) {
